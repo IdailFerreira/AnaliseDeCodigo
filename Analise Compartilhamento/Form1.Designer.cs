7452ead6871ab7475ad78bef538781d2eb6525ea
Idail Ferreira <neto_br_8@hotmail.com>
2021-06-03 18:44:19 -0400

    Modificações feitas por neto_br_8@hotmail.com, mudado propriedades do formulario, adicionado outro botão com função de mensagem.

--- Analise Compartilhamento/Form1.Designer.cs
+++ Analise Compartilhamento/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace Analise_Compartilhamento
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,12 +43,24 @@ namespace Analise_Compartilhamento
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Mensagem 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // frm_inicial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 52);
+            this.ClientSize = new System.Drawing.Size(203, 52);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
             this.Name = "frm_inicial";
             this.Text = "Formulario";
             this.ResumeLayout(false);
@@ -57,6 +70,7 @@ namespace Analise_Compartilhamento
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
